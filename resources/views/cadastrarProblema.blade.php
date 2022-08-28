0ff37e600bfc319ef289939dee77e774559b951c
unknown <vitorribeiro9454@gmail.com>
2022-08-28 10:45:23 -0300

    mapa de cadastrar

--- resources/views/cadastrarProblema.blade.php
+++ resources/views/cadastrarProblema.blade.php
@@ -28,21 +28,29 @@
                     <div class="form-group">
                         
                         <label for="exampleFormControlInput1">Localização:</label>
-    </div>
-<style>
+                    </div>
+                    <style>
+                    #map {
 
+                        width: 100%;
+                        height: 100px;
+                        background-color: grey;
 
-</style>
-            <div id="map"></div>
-<script>
+                    }
 
-                function initMap() {
-                var macc = {lat: -6.479984036211917, lng: -35.43179852420206};
-                var map = new google.maps.Map(
-                document.getElementById('map'), {zoom: 15, center: macc});
-                var marker = new google.maps.Marker({position: macc, map: map});}
+                    </style>
+                    <div id="map"></div>
+                    <script>
 
- </script>
+                        function initMap() {
+                            var macc = {lat: -6.479984036211917, lng: -35.43179852420206};
+                            var map = new google.maps.Map(
+                            document.getElementById('map'), {zoom: 15, center: macc});
+                            var marker = new google.maps.Marker({position: macc, map: map});
+                        }
+
+                    </script>
+                    <br><br>
 
                     <div class="form-group">
                     <label for="exampleFormControlTextarea1">Descrição:</label>
